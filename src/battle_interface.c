bd412341a2a60f3fb88836ca817d80a059f1b8a5
PKMN2K <revolutionbummer.blog@gmail.com>
2026-09-20 15:17:50 -0400

    fix: remove obsolete move description prompt config

--- src/battle_interface.c
+++ src/battle_interface.c
@@ -3196,9 +3196,6 @@ void TryToAddMoveInfoWindow(void)
     if (!B_SHOW_MOVE_DESCRIPTION)
         return;
 
-    if (!B_SHOW_MOVE_DESCRIPTION_PROMPT)
-        return;
-
     if (B_MOVE_DESCRIPTION_BUTTON == L_BUTTON && gSaveBlock2Ptr->optionsButtonMode == OPTIONS_BUTTON_MODE_L_EQUALS_A)
         return;
 
