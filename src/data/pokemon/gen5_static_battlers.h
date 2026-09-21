3c431036190366d0ca67d6d1721a1f86f46c6665
PKMN2K <revolutionbummer.blog@gmail.com>
2026-09-21 16:20:48 -0400

    gen5 battlers: add src/data/pokemon/gen5_static_battlers.h

---
+++ src/data/pokemon/gen5_static_battlers.h
@@ -0,0 +1,13 @@
+// Add one line per species that should use a static 96x96 Gen 5-style
+// battle sprite. The folder must exist under graphics/pokemon_gen5_static/
+//
+// Example:
+// GEN5_STATIC_BATTLER(BULBASAUR, bulbasaur)
+//
+// Each folder contains:
+//   front.png   - 96x96 indexed front sprite
+//   back.png    - 96x96 indexed back sprite
+//   normal.pal  - normal 16-colour palette
+//   shiny.pal   - shiny 16-colour palette
+//
+// Keep this list empty until the corresponding assets have been added.
