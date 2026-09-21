c8d7684d39f2672e10138612dfa221d9c358af59
PKMN2K <revolutionbummer.blog@gmail.com>
2026-09-20 21:24:17 -0400

    fix: use Expansion game-version guard in swsh_summary_screen.h

--- include/swsh_summary_screen.h
+++ include/swsh_summary_screen.h
@@ -4,7 +4,7 @@
 #include "main.h"
 
 // turn on and off the SwSh summary screen
-#define SWSH_SUMMARY_SCREEN                           IS_EMERALD
+#define SWSH_SUMMARY_SCREEN                           !IS_FRLG
 
 // constants
 #define SWSH_MAX_MOVE_DESCRIPTION_LENGTH              100       // this only needs to be updated if you use auto-formatting
