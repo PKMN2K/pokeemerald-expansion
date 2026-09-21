c1dfec62bb188968b04f1bd433ab3862f4f5246f
PKMN2K <revolutionbummer.blog@gmail.com>
2026-09-20 21:24:24 -0400

    fix: use Expansion game-version guard in unbound_start_menu.c

--- src/unbound_start_menu.c
+++ src/unbound_start_menu.c
@@ -1,6 +1,6 @@
 #include "global.h"
 
-#if IS_EMERALD
+#if !IS_FRLG
 #include "assertf.h"
 #include "battle_pyramid.h"
 #include "battle_pyramid_bag.h"
@@ -1399,4 +1399,4 @@ static bool32 shouldIconsObjWinMask(void)
     else return FALSE;
 }
 
-#endif // IS_EMERALD
+#endif // !IS_FRLG
