adb20843984d7f56273869ec54541279f20bc119
PKMN2K <revolutionbummer.blog@gmail.com>
2026-09-20 20:54:52 -0400

    port: reserve existing option padding for Worped UI settings

--- include/global.h
+++ include/global.h
@@ -602,8 +602,14 @@ struct SaveBlock2
              u16 optionsBattleStyle:1; // OPTIONS_BATTLE_STYLE_[SHIFT/SET]
              u16 optionsBattleSceneOff:1; // whether battle animations are disabled
              u16 regionMapZoom:1; // whether the map is zoomed in
-             //u16 padding1:4;
-             //u16 padding2;
+             // Use the remaining option padding for Worped UI preferences.
+             u16 w_opAutoRun:1;
+             u16 w_opDifficulty:2;
+             u16 w_opMatchCall:1;
+             u16 w_opBoxMode:1;
+             u16 w_opBallPrompt:1;
+             u16 w_opCatchMode:1;
+             u16 w_opFontType:1;
     /*0x18*/ struct Pokedex pokedex;
     /*0x90*/ u8 filler_90[0x8];
     /*0x98*/ struct Time localTimeOffset;
