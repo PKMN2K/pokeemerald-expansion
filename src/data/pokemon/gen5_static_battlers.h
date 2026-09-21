d232262124950767c3acc6540b1ba73c5bbffbf8
PKMN2K <revolutionbummer.blog@gmail.com>
2026-09-21 16:33:30 -0400

    gen5 battlers: simplify species registry

--- src/data/pokemon/gen5_static_battlers.h
+++ src/data/pokemon/gen5_static_battlers.h
@@ -1,13 +1,11 @@
-// Add one line per species that should use a static 96x96 Gen 5-style
-// battle sprite. The folder must exist under graphics/pokemon_gen5_static/
+// Register each species that should use static 96x96 Gen 5-style
+// battle sprites.
 //
-// Example:
-// GEN5_STATIC_BATTLER(BULBASAUR, bulbasaur)
+// First declare its four assets in:
+//   src/data/graphics/gen5_static_battlers.h
 //
-// Each folder contains:
-//   front.png   - 96x96 indexed front sprite
-//   back.png    - 96x96 indexed back sprite
-//   normal.pal  - normal 16-colour palette
-//   shiny.pal   - shiny 16-colour palette
+// Then add one line here:
+//
+// GEN5_STATIC_BATTLER(BULBASAUR)
 //
 // Keep this list empty until the corresponding assets have been added.
