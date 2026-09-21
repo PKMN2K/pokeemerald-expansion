fd0932ec090d9f069ed408d4981af312c39794e6
PKMN2K <revolutionbummer.blog@gmail.com>
2026-09-20 21:24:09 -0400

    fix: use Expansion game-version guard in swsh_storage_system.h

--- include/swsh_storage_system.h
+++ include/swsh_storage_system.h
@@ -3,7 +3,7 @@
 
 #include "constants/form_change_types.h"
 
-#define SWSH_STORAGE_SYSTEM IS_EMERALD
+#define SWSH_STORAGE_SYSTEM !IS_FRLG
 
 void ShowPokemonStorageSystemPC_SwSh(void);
 void ShowPokemonPCFromParty_SwSh(void);
