7211160bb6ebc3e10709b50e8909a0c75773d3a1
PKMN2K <revolutionbummer.blog@gmail.com>
2026-09-20 20:55:25 -0400

    port: route storage entry points to Worpbane SwSh UI

--- src/pokemon_storage_system.c
+++ src/pokemon_storage_system.c
@@ -46,6 +46,7 @@
 #include "constants/pokemon_icon.h"
 #include "chooseboxmon.h"
 #include "party_menu.h"
+#include "swsh_storage_system.h"
 
 /*
     NOTE: This file is large. Some general groups of functions have
@@ -1621,6 +1622,12 @@ static void Task_PCMainMenu(u8 taskId)
 
 void ShowPokemonStorageSystemPC(void)
 {
+    if (SWSH_STORAGE_SYSTEM)
+    {
+        ShowPokemonStorageSystemPC_SwSh();
+        return;
+    }
+
     u8 taskId = CreateTask(Task_PCMainMenu, 80);
     gTasks[taskId].tState = 0;
     gTasks[taskId].tSelectedOption = 0;
@@ -6970,12 +6977,24 @@ static void ReshowDisplayMon(void)
 
 void SetMonFormPSS(struct BoxPokemon *boxMon, enum FormChanges method)
 {
+    if (SWSH_STORAGE_SYSTEM)
+    {
+        SetMonFormPSS_SwSh(boxMon, method);
+        return;
+    }
+
     if (TryBoxMonFormChange(boxMon, method))
         sRefreshDisplayMonGfx = TRUE;
 }
 
 void SetMonFormPSS_ItemHold(struct BoxPokemon *boxMon)
 {
+    if (SWSH_STORAGE_SYSTEM)
+    {
+        SetMonFormPSS_ItemHold_SwSh(boxMon);
+        return;
+    }
+
     if (TryBoxMonFormChange(boxMon, FORM_CHANGE_ITEM_HOLD))
         sRefreshDisplayMonGfx = TRUE;
     UpdateSpeciesSpritePSS(boxMon);
@@ -10046,6 +10065,12 @@ static void TilemapUtil_Draw(u8 id)
 
 void UpdateSpeciesSpritePSS(struct BoxPokemon *boxMon)
 {
+    if (SWSH_STORAGE_SYSTEM)
+    {
+        UpdateSpeciesSpritePSS_SwSh(boxMon);
+        return;
+    }
+
     enum Species species = GetBoxMonData(boxMon, MON_DATA_SPECIES);
     bool32 isShiny = GetBoxMonData(boxMon, MON_DATA_IS_SHINY);
     u32 pid = GetBoxMonData(boxMon, MON_DATA_PERSONALITY);
@@ -10081,6 +10106,12 @@ void UpdateSpeciesSpritePSS(struct BoxPokemon *boxMon)
 
 void ChooseMonFromStorage(void)
 {
+    if (SWSH_STORAGE_SYSTEM)
+    {
+        ChooseMonFromStorage_SwSh();
+        return;
+    }
+
     EnterPokeStorage(OPTION_SELECT_MON);
 }
 
