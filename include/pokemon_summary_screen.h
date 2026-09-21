54821f9cbfff7e0d23e620222d5817338977eecb
PKMN2K <revolutionbummer.blog@gmail.com>
2026-09-20 21:03:32 -0400

    port: keep extra summary page Emerald-only

--- include/pokemon_summary_screen.h
+++ include/pokemon_summary_screen.h
@@ -40,7 +40,9 @@ enum PokemonSummaryScreenPage
     PSS_PAGE_SKILLS,
     PSS_PAGE_BATTLE_MOVES,
     PSS_PAGE_CONTEST_MOVES,
+#if IS_EMERALD
     PSS_PAGE_CONDITIONS,
+#endif
     PSS_PAGE_COUNT,
 };
 
