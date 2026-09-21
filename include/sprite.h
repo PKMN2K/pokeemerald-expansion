a91cf2721e55153783e0c3d47f4ae90210f85def
PKMN2K <revolutionbummer.blog@gmail.com>
2026-09-21 15:54:58 -0400

    Expose dynamic OBJ tile reallocation

--- include/sprite.h
+++ include/sprite.h
@@ -310,6 +310,7 @@ u16 LoadSpriteSheet(const struct SpriteSheet *sheet);
 u16 LoadSpriteSheetByTemplate(const struct SpriteTemplate *template, u32 frame, s32 offset);
 void LoadSpriteSheets(const struct SpriteSheet *sheets);
 s16 AllocSpriteTiles(u16 tileCount);
+s16 ReallocSpriteTiles(struct Sprite *sprite, u16 tileCount);
 bool32 CanAllocSpriteTiles(u16 tileCount);
 void FreeSpriteTilesByTag(u16 tag);
 void FreeSpriteTileRanges(void);
