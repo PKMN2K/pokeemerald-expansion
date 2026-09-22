ef47ea1e0b54eeb8b6deecb02c39343033ccf2d2
PKMN2K <revolutionbummer.blog@gmail.com>
2026-09-22 09:12:52 -0400

    Center HGSS bag registered label

--- src/item_menu.c
+++ src/item_menu.c
@@ -1140,7 +1140,8 @@ static void DrawHgssBagRegisteredMarker(u8 windowId, u8 y)
     FillWindowPixelRect(windowId, PIXEL_FILL(HGSS_BAG_CELL_ACTIVE_COLOR), left + 1, top + height - 1, width - 2, 1);
     FillWindowPixelRect(windowId, PIXEL_FILL(HGSS_BAG_CELL_ACTIVE_COLOR), left, top + 1, 1, height - 2);
     FillWindowPixelRect(windowId, PIXEL_FILL(HGSS_BAG_CELL_ACTIVE_COLOR), left + width - 1, top + 1, 1, height - 2);
-    BagMenu_Print(windowId, FONT_NARROW, sText_Registered, left + 2, y + 1,
+    BagMenu_Print(windowId, FONT_NARROW, sText_Registered,
+                  left + GetStringCenterAlignXOffset(FONT_NARROW, sText_Registered, width), y + 1,
                   0, 0, TEXT_SKIP_DRAW, COLORID_POCKET_TITLE);
 }
 
