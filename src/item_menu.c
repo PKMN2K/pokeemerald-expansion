482a60051324d2790e02bfadbe6bf2c884306da7
PKMN2K <revolutionbummer.blog@gmail.com>
2026-09-22 12:35:06 -0400

    Restore HGSS yes no frame

--- src/item_menu.c
+++ src/item_menu.c
@@ -3080,10 +3080,6 @@ static void DrawHgssBagYesNoMenu(void)
     u8 height = GetWindowAttribute(windowId, WINDOW_HEIGHT) * 8;
 
     FillWindowPixelBuffer(windowId, PIXEL_FILL(1));
-    FillWindowPixelRect(windowId, PIXEL_FILL(TEXT_COLOR_RED), 1, 0, width - 2, 1);
-    FillWindowPixelRect(windowId, PIXEL_FILL(TEXT_COLOR_RED), 1, height - 1, width - 2, 1);
-    FillWindowPixelRect(windowId, PIXEL_FILL(TEXT_COLOR_RED), 0, 1, 1, height - 2);
-    FillWindowPixelRect(windowId, PIXEL_FILL(TEXT_COLOR_RED), width - 1, 1, 1, height - 2);
     BagMenu_Print(windowId, FONT_NARROW, gText_YesNo, 8, 1, 0, 0, TEXT_SKIP_DRAW, COLORID_POCKET_NAME);
     {
         u8 cursorY = 3 + sHgssBagYesNoChoice * 16;
@@ -3092,6 +3088,12 @@ static void DrawHgssBagYesNoMenu(void)
         FillWindowPixelRect(windowId, PIXEL_FILL(TEXT_COLOR_RED), 2, cursorY + 1, 4, 9);
         FillWindowPixelRect(windowId, PIXEL_FILL(TEXT_COLOR_RED), 3, cursorY + 10, 2, 1);
     }
+
+    // Keep the HGSS frame above both 16 px text rows.
+    FillWindowPixelRect(windowId, PIXEL_FILL(TEXT_COLOR_RED), 1, 0, width - 2, 1);
+    FillWindowPixelRect(windowId, PIXEL_FILL(TEXT_COLOR_RED), 1, height - 1, width - 2, 1);
+    FillWindowPixelRect(windowId, PIXEL_FILL(TEXT_COLOR_RED), 0, 1, 1, height - 2);
+    FillWindowPixelRect(windowId, PIXEL_FILL(TEXT_COLOR_RED), width - 1, 1, 1, height - 2);
     CopyWindowToVram(windowId, COPYWIN_GFX);
 }
 
