3c23009d9bcc7851b2f4306356d64d0a27c41e10
PKMN2K <revolutionbummer.blog@gmail.com>
2026-09-22 11:48:05 -0400

    Align HGSS registered label vertically

--- src/item_menu.c
+++ src/item_menu.c
@@ -1138,7 +1138,7 @@ static void DrawHgssBagRegisteredMarker(u8 windowId, u8 y)
     FillWindowPixelRect(windowId, PIXEL_FILL(HGSS_BAG_CELL_ACTIVE_COLOR), left, top + 1, 1, height - 2);
     FillWindowPixelRect(windowId, PIXEL_FILL(HGSS_BAG_CELL_ACTIVE_COLOR), left + width - 1, top + 1, 1, height - 2);
     BagMenu_Print(windowId, FONT_NARROW, sText_Registered,
-                  left + GetStringCenterAlignXOffset(FONT_NARROW, sText_Registered, width), y + 1,
+                  left + GetStringCenterAlignXOffset(FONT_NARROW, sText_Registered, width), y,
                   0, 0, TEXT_SKIP_DRAW, COLORID_POCKET_TITLE);
 }
 
