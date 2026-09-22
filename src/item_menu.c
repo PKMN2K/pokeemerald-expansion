fae993bca2db2162f71bb11064298b895b5d48ab
PKMN2K <revolutionbummer.blog@gmail.com>
2026-09-21 22:25:23 -0400

    Apply HGSS bag sprites and gender palettes

--- src/item_menu.c
+++ src/item_menu.c
@@ -876,7 +876,10 @@ static bool8 LoadBagMenu_Graphics(void)
         gBagMenu->graphicsLoadState++;
         break;
     case 4:
-        LoadSpritePalette(&gBagPaletteTable);
+        if (IsWallysBag() == TRUE || gSaveBlock2Ptr->playerGender == MALE)
+            LoadSpritePalette(&gBagMalePaletteTable);
+        else
+            LoadSpritePalette(&gBagFemalePaletteTable);
         gBagMenu->graphicsLoadState++;
         break;
     default:
