d1fcc4f98681c5040875e987b4e01471d0354ede
PKMN2K <revolutionbummer.blog@gmail.com>
2026-09-26 02:08:15 -0400

    Build authentic HGSS Cry assets

--- graphics_file_rules.mk
+++ graphics_file_rules.mk
@@ -18,6 +18,7 @@ GEN4UIHGSSLIST := tools/gen4_ui/make_hgss_pokedex_list_member_000.py
 GEN4UIHGSSSTATS := tools/gen4_ui/make_hgss_pokedex_stats.py
 GEN4UIHGSSEVO := tools/gen4_ui/make_hgss_pokedex_evolution.py
 GEN4UIHGSSFORMS := tools/gen4_ui/make_hgss_pokedex_forms.py
+GEN4UIHGSSCRY := tools/gen4_ui/make_hgss_pokedex_cry.py
 
 types := none normal fight flying poison ground rock bug ghost steel mystery fire water grass electric psychic ice dragon dark fairy stellar
 contest_types := cool beauty cute smart tough
@@ -44,6 +45,9 @@ $(GEN4UIGFXDIR)/hgss_pokedex_evolution_gba.png: $(GEN4UIHGSSEVO)
 	python3 $< $@
 
 $(GEN4UIGFXDIR)/hgss_pokedex_forms_gba.png: $(GEN4UIHGSSFORMS)
+	python3 $< $@
+
+$(GEN4UIGFXDIR)/hgss_pokedex_cry_gba.png: $(GEN4UIHGSSCRY)
 	python3 $< $@
 
 # The live HGSS Pokédex background reserves BG palette entries 128..140.
@@ -83,6 +87,12 @@ $(GEN4UIGFXDIR)/hgss_pokedex_forms_gba.tiles.8bpp: $(GEN4UIGFXDIR)/hgss_pokedex_
 	python3 $(GEN4UIPACK) $(word 2,$^) $< --tiles $@ --palette-base 128
 
 $(GEN4UIGFXDIR)/hgss_pokedex_forms_gba.tilemap.bin: $(GEN4UIGFXDIR)/hgss_pokedex_forms_gba.8bpp $(GEN4UIGFXDIR)/hgss_pokedex_forms_gba.png $(GEN4UIPACK)
+	python3 $(GEN4UIPACK) $(word 2,$^) $< --tilemap $@ --palette-base 128
+
+$(GEN4UIGFXDIR)/hgss_pokedex_cry_gba.tiles.8bpp: $(GEN4UIGFXDIR)/hgss_pokedex_cry_gba.8bpp $(GEN4UIGFXDIR)/hgss_pokedex_cry_gba.png $(GEN4UIPACK)
+	python3 $(GEN4UIPACK) $(word 2,$^) $< --tiles $@ --palette-base 128
+
+$(GEN4UIGFXDIR)/hgss_pokedex_cry_gba.tilemap.bin: $(GEN4UIGFXDIR)/hgss_pokedex_cry_gba.8bpp $(GEN4UIGFXDIR)/hgss_pokedex_cry_gba.png $(GEN4UIPACK)
 	python3 $(GEN4UIPACK) $(word 2,$^) $< --tilemap $@ --palette-base 128
 
 # gbagfx first converts the indexed source PNG into a full 8bpp tile stream.
