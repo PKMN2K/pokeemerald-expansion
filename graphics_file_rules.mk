adb83cb6d77b1d9550951b1d650655564e3c469b
PKMN2K <revolutionbummer.blog@gmail.com>
2026-09-26 01:06:01 -0400

    Build compact HGSS Pokédex production layout

--- graphics_file_rules.mk
+++ graphics_file_rules.mk
@@ -27,6 +27,9 @@ $(GEN4UIGFXDIR)/renderer_test.png: $(GEN4UITESTFIXTURE)
 $(GEN4UIGFXDIR)/hgss_pokedex_member_020.png: $(GEN4UIHGSSDEX)
 	python3 $< $@
 
+$(GEN4UIGFXDIR)/hgss_pokedex_info_gba.png: $(GEN4UIHGSSDEX)
+	python3 $< --gba-layout $@
+
 # The live HGSS Pokédex background reserves BG palette entries 128..140.
 # Shifting tile pixel indices keeps the legacy Pokédex palettes (0..95 and 240..255)
 # available for text, windows, and other existing layers.
@@ -34,6 +37,12 @@ $(GEN4UIGFXDIR)/hgss_pokedex_member_020.tiles.8bpp: $(GEN4UIGFXDIR)/hgss_pokedex
 	python3 $(GEN4UIPACK) $(word 2,$^) $< --tiles $@ --palette-base 128
 
 $(GEN4UIGFXDIR)/hgss_pokedex_member_020.tilemap.bin: $(GEN4UIGFXDIR)/hgss_pokedex_member_020.8bpp $(GEN4UIGFXDIR)/hgss_pokedex_member_020.png $(GEN4UIPACK)
+	python3 $(GEN4UIPACK) $(word 2,$^) $< --tilemap $@ --palette-base 128
+
+$(GEN4UIGFXDIR)/hgss_pokedex_info_gba.tiles.8bpp: $(GEN4UIGFXDIR)/hgss_pokedex_info_gba.8bpp $(GEN4UIGFXDIR)/hgss_pokedex_info_gba.png $(GEN4UIPACK)
+	python3 $(GEN4UIPACK) $(word 2,$^) $< --tiles $@ --palette-base 128
+
+$(GEN4UIGFXDIR)/hgss_pokedex_info_gba.tilemap.bin: $(GEN4UIGFXDIR)/hgss_pokedex_info_gba.8bpp $(GEN4UIGFXDIR)/hgss_pokedex_info_gba.png $(GEN4UIPACK)
 	python3 $(GEN4UIPACK) $(word 2,$^) $< --tilemap $@ --palette-base 128
 
 # gbagfx first converts the indexed source PNG into a full 8bpp tile stream.
